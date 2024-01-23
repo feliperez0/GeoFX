00349eff32800159072b477ff93cde32048e3277
feliperez0 <feliperezmorales06@gmail.com>
2024-01-23 10:08:36 +0000

    1.0.1

--- src/main/java/dad/geofx/api/MainController.java
+++ src/main/java/dad/geofx/api/MainController.java
@@ -17,107 +17,86 @@ import javafx.scene.image.ImageView;
 public class MainController implements Initializable {
 
 	GeoService service = new GeoService();
-	
+
 	GeoServiceIpapi serviceIpapi = new GeoServiceIpapi();
-	
-	 @FXML
-	    private Label asnLabel;
 
-	    @FXML
-	    private ImageView banderaImage;
+	@FXML
+	private Label asnLabel;
 
-	    @FXML
-	    private Label ciudadLabel;
+	@FXML
+	private ImageView banderaImage;
 
-	    @FXML
-	    private CheckBox crawlerCheck;
+	@FXML
+	private Label ciudadLabel;
 
-	    @FXML
-	    private Label hostnameLabel;
+	@FXML
+	private CheckBox crawlerCheck;
 
-	    @FXML
-	    private Label ipAddressLabel;
+	@FXML
+	private Label hostnameLabel;
 
-	    @FXML
-	    private TextField ipText;
+	@FXML
+	private Label ipAddressLabel;
 
-	    @FXML
-	    private Label ispLabel;
+	@FXML
+	private TextField ipText;
 
-	    @FXML
-	    private Label latitudLabel;
+	@FXML
+	private Label ispLabel;
 
-	    @FXML
-	    private Label lenguajeLabel;
+	@FXML
+	private Label latitudLabel;
 
-	    @FXML
-	    private Label localizacionLabel;
+	@FXML
+	private Label lenguajeLabel;
 
-	    @FXML
-	    private Label longitudLabel;
+	@FXML
+	private Label localizacionLabel;
 
-	    @FXML
-	    private Label monedaLabel;
+	@FXML
+	private Label longitudLabel;
 
-	    @FXML
-	    private Label potentialLabel;
+	@FXML
+	private Label monedaLabel;
 
-	    @FXML
-	    private CheckBox proxyCheck;
+	@FXML
+	private Label potentialLabel;
 
-	    @FXML
-	    private Label telefonoLabel;
+	@FXML
+	private CheckBox proxyCheck;
 
-	    @FXML
-	    private Label threatLabel;
+	@FXML
+	private Label telefonoLabel;
 
-	    @FXML
-	    private Label tiempoLabel;
+	@FXML
+	private Label threatLabel;
 
-	    @FXML
-	    private CheckBox torCheck;
+	@FXML
+	private Label tiempoLabel;
 
-	    @FXML
-	    private Label typeLabel;
+	@FXML
+	private CheckBox torCheck;
 
-	    @FXML
-	    private BorderPane view;
+	@FXML
+	private Label typeLabel;
 
-	    @FXML
-	    private Label zipLabel;
+	@FXML
+	private BorderPane view;
 
-	    @FXML
-	    void onCheckIp(ActionEvent event) {
+	@FXML
+	private Label zipLabel;
 
-	        GeoIpapiResponse ipApiResponse = serviceIpapi.getIpInfo(ipText.getText());
-			
-			latitudLabel.setText(String.format("%.6f", ipApiResponse.getLatitude()));
-			longitudLabel.setText(String.format("%.6f", ipApiResponse.getLongitude()));
-			localizacionLabel.setText(ipApiResponse.getCountry_name()+" ("+ipApiResponse.getCountry_code()+")");
-			Image flags = new Image(getClass().getResourceAsStream("/flag-icons/64x42/" +ipApiResponse.getCountry_code()+ ".png"));
-			banderaImage.setImage(flags);
-			ciudadLabel.setText(ipApiResponse.getCity());
-			zipLabel.setText(ipApiResponse.getZip());
-//			lenguajeLabel.setText(ipApiResponse.getLocation().getLanguages());
-			tiempoLabel.setText(ipApiResponse.getTime_zone().getCode());
-			telefonoLabel.setText("+"+ipApiResponse.getLocation().getCalling_code());
-			monedaLabel.setText(ipApiResponse.getCurrency().getName()+" ("+ipApiResponse.getCurrency().getSymbol()+")");
-			ipAddressLabel.setText(ipText.getText());
-			ispLabel.setText(ipApiResponse.getConnection().getIsp());
-			typeLabel.setText(ipApiResponse.getType());
-			asnLabel.setText(ipApiResponse.getConnection().getAsn()+"");
-			hostnameLabel.setText(ipApiResponse.getHostname());
-			
-			if(ipApiResponse.getSecurity().isIs_proxy())
-				proxyCheck.setSelected(true);
-			if(ipApiResponse.getSecurity().isIs_tor())
-				torCheck.setSelected(true);
-			if(ipApiResponse.getSecurity().isIs_crawler())
-				crawlerCheck.setSelected(true);
-			
-			threatLabel.setText(ipApiResponse.getSecurity().getThreat_level());
-	    	
-	    }
+	@FXML
+	void onCheckIp(ActionEvent event) {
+
+		if(ipText.getText().equals("")) {
+			ipText.setText(service.getIpAddress());
+			changedDates(service.getIpAddress());
+		} else {
+			changedDates(ipText.getText());
+		}
+		
+	}
 
 	public MainController() {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/View.fxml"));
@@ -132,37 +111,58 @@ public class MainController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
-        GeoIpapiResponse ipApiResponse = serviceIpapi.getIpInfo(service.getIpAddress());
-		
 		ipText.setText(service.getIpAddress());
+		changedDates(service.getIpAddress());
+
+	}
+	
+	public void changedDates(String ipAddress) {
+		
+		GeoIpapiResponse ipApiResponse = serviceIpapi.getIpInfo(ipAddress);			
+		
 		latitudLabel.setText(String.format("%.6f", ipApiResponse.getLatitude()));
 		longitudLabel.setText(String.format("%.6f", ipApiResponse.getLongitude()));
-		localizacionLabel.setText(ipApiResponse.getCountry_name()+" ("+ipApiResponse.getCountry_code()+")");
-		Image flags = new Image(getClass().getResourceAsStream("/flag-icons/64x42/" +ipApiResponse.getCountry_code()+ ".png"));
+		localizacionLabel.setText(ipApiResponse.getCountry_name() + " (" + ipApiResponse.getCountry_code() + ")");
+		Image flags = new Image(getClass().getResourceAsStream("/flag-icons/64x42/" + ipApiResponse.getCountry_code() + ".png"));
 		banderaImage.setImage(flags);
 		ciudadLabel.setText(ipApiResponse.getCity());
 		zipLabel.setText(ipApiResponse.getZip());
-//		lenguajeLabel.setText(ipApiResponse.getLocation().getLanguages());
+        lenguajeLabel.setText(ipApiResponse.getLocation().getLanguages().get(0).getName());
 		tiempoLabel.setText(ipApiResponse.getTime_zone().getCode());
-		telefonoLabel.setText("+"+ipApiResponse.getLocation().getCalling_code());
-		monedaLabel.setText(ipApiResponse.getCurrency().getName()+" ("+ipApiResponse.getCurrency().getSymbol()+")");
+		telefonoLabel.setText("+" + ipApiResponse.getLocation().getCalling_code());
+		monedaLabel.setText(ipApiResponse.getCurrency().getName() + " (" + ipApiResponse.getCurrency().getSymbol() + ")");
 		ipAddressLabel.setText(ipText.getText());
-		ispLabel.setText(ipApiResponse.getConnection().getIsp());
+		ispLabel.setText(" "+ipApiResponse.getConnection().getIsp());
 		typeLabel.setText(ipApiResponse.getType());
-		asnLabel.setText(ipApiResponse.getConnection().getAsn()+"");
+		asnLabel.setText(ipApiResponse.getConnection().getAsn() + "");
 		hostnameLabel.setText(ipApiResponse.getHostname());
-		
-		if(ipApiResponse.getSecurity().isIs_proxy())
+
+		if (ipApiResponse.getSecurity().isIs_proxy())
 			proxyCheck.setSelected(true);
-		if(ipApiResponse.getSecurity().isIs_tor())
+		if (ipApiResponse.getSecurity().isIs_tor())
 			torCheck.setSelected(true);
-		if(ipApiResponse.getSecurity().isIs_crawler())
+		if (ipApiResponse.getSecurity().isIs_crawler())
 			crawlerCheck.setSelected(true);
-		
+
 		threatLabel.setText(ipApiResponse.getSecurity().getThreat_level());
 
+		switch (ipApiResponse.getSecurity().getThreat_level()) {
+		case "low":
+			potentialLabel.setText("This IP is safe. No threats have been detected.");
+			break;
+		case "medium":
+			potentialLabel.setText("Potential threaths detected. This IP may be unsafe.");
+			break;
+		case "high":
+			potentialLabel.setText("Defintive threats detected. This IP is unsafe.");
+			break;
+		default:
+			potentialLabel.setText("Threat level unknown");
+			break;
+		}
+		
 	}
-
+	
 	public BorderPane getView() {
 		return view;
 	}
